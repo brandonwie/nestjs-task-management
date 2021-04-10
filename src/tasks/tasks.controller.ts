5cecbf404068b70c4aff2250ac2f592e4704a57c
Neo Wie <it.brandonwie@gmail.com>
2021-04-11 01:51:38 +0800

    feat(TypeORM): connect module to PostgreSQL DB using TypeORM

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,13 +9,15 @@ import {
   UsePipes,
   ValidationPipe,
   BadRequestException,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Query } from '@nestjs/common';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
 
 /**
  * @route /task
@@ -24,45 +26,16 @@ import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  /** Get all tasks
-   * @route /tasks
-   * @pipe ValidationPipe
-   * @param filterDto
-   * @returns Task
-   */
   @Get()
-  getTasks(
-    @Query(
-      // just pass "ValidationPipe" to use v.7(simplified)
-      new ValidationPipe({
-        exceptionFactory: (errors) => new BadRequestException(errors),
-      }),
-    )
-    filterDto: GetTasksFilterDto,
-  ): Task[] {
-    if (Object.keys(filterDto).length) {
-      return this.tasksService.getTasksWithFilters(filterDto);
-    } else {
-      return this.tasksService.getAllTasks();
-    }
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto) {
+    return this.tasksService.getTasks(filterDto);
   }
 
-  /** Get a task by ID
-   * @route /tasks/:id
-   * @param id
-   * @returns Task matches ID
-   */
   @Get('/:id')
-  async getTaskById(@Param('id') id: string): Promise<Task> {
+  async getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
     return this.tasksService.getTaskById(id);
   }
 
-  /** Create a task
-   * @route /tasks
-   * @pipe ValidationPipe
-   * @param createTaskDto
-   * @returns Task created
-   */
   @Post()
   @UsePipes(
     // just pass "ValidationPipe" to use v.7(simplified)
@@ -70,30 +43,20 @@ export class TasksController {
       exceptionFactory: (errors) => new BadRequestException(errors),
     }),
   )
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto);
   }
 
-  /** Delete a task by ID
-   * @route tasks/:id
-   * @param id
-   */
   @Delete('/:id')
-  deleteTaskById(@Param('id') id: string): void {
-    this.tasksService.deleteTaskById(id);
+  deleteTaskById(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.tasksService.deleteTask(id);
   }
 
-  /** Update a task by ID
-   * @route tasks/:id/status
-   * @param id
-   * @param status
-   * @returns Task updated
-   */
   @Patch('/:id/status')
   updateTaskStatus(
-    @Param('id') id: string,
+    @Param('id', ParseIntPipe) id: number,
     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  ): Task {
+  ): Promise<Task> {
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
