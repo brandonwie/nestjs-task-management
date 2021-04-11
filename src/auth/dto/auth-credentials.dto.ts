70c277b79d33f1b82a293501816648f220f7b074
Neo Wie <it.brandonwie@gmail.com>
2021-04-11 18:01:42 +0800

    feat(/auth): username, password validator

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, Length, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @Length(4, 20)
+  username: string;
+
+  @IsString()
+  @Length(8, 20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too week',
+  })
+  password: string;
+}
